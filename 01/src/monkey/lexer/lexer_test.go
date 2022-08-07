a8987bc4d9f5000a1705417d6047801433b9cbd0
MB <michael.dburke56@gmail.com>
2022-08-06 20:31:05 -0500

    Partially implement tests.

--- 01/src/monkey/lexer/lexer_test.go
+++ 01/src/monkey/lexer/lexer_test.go
@@ -5,3 +5,24 @@ import (
 
 	"monkey/token"
 )
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},:`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+}
