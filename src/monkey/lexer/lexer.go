0c2e8b42b2d4b125d14879702ef52bec37c65eb4
MB <michael.dburke56@gmail.com>
2022-08-17 18:59:24 -0500

    Update lexer w/ helper functions and extended use case.

--- src/monkey/lexer/lexer.go
+++ src/monkey/lexer/lexer.go
@@ -18,6 +18,8 @@ func New(input string) *Lexer {
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhitespcae()
+
 	switch l.ch {
 	case '/':
 		tok = newToken(token.SLASH, l.ch)
@@ -42,10 +44,26 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.readChar()
 	return tok
+}
+
+func (l *Lexer) readChar() {
+
 }
 
 func (l *Lexer) readChar() {
